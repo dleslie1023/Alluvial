58fb59ab9efbc6cd4d49566c9f6a56791f0099f0
David Leslie <dave@localhost.localdomain>
2015-04-12 15:01:11 -0400

    Basic Soundcloud functionality to query tracks and request downloadable tracks with SCHandler class

---
+++ schandler.cpp
@@ -0,0 +1,178 @@
+#include "schandler.h"
+#include <QCoreApplication>
+#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+
+#include <QUrlQuery>
+
+#include <QStandardPaths>
+#include <QFileInfo>
+#include <QFile>
+
+
+
+SCHandler::SCHandler()
+{
+    results = QJsonArray();
+
+}
+
+
+SCHandler::~SCHandler()
+{
+
+}
+
+
+//category: artist, title, user,
+int SCHandler::query(QString value, QString key = "title"){
+    last_search = QJsonDocument(QJsonDocument::fromJson(QString("{"+key+","+value+"}")));
+    // create custom temporary event loop on stack
+    QEventLoop eventLoop;
+    QUrl url(SC_TRACKS_URL);
+    QUrlQuery query;
+    query.addQueryItem("client_id", SC_CLIENT_ID);
+
+    //switch for request type
+    //loop to pull out queries
+
+    query.addQueryItem(key, value);
+    query.addQueryItem("downloadable", "true");
+
+
+    url.setQuery(query.query());
+
+    // "quit()" the event-loop, when the network request "finished()"
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+    // the HTTP request
+    QNetworkRequest req(url);
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+    if (reply->error() == QNetworkReply::NoError) {
+        QJsonParseError err;
+        QJsonDocument jsondoc = QJsonDocument(QJsonDocument::fromJson(QString(reply->readAll()).toUtf8(), &err)); //raw string to qtstring to bytecode to jsondoc fucking shit
+        results = jsondoc.array(); //take the pile of responses and make them an array so you can fucking do something with the
+        return results.size();
+
+    }
+    else {
+        //failure
+        qDebug() << "Failed query: " <<reply->errorString();
+        delete reply;
+        return -1;
+    }
+}
+
+//not working
+//void SCHandler::connect(QString uname, QString pword){
+//    QEventLoop eventLoop;
+//    QUrl url("https://api.soundcloud.com/tracks");
+//    QUrlQuery query;
+
+//    //client_id
+//    query.addQueryItem("client_id", SC_CLIENT_ID);
+//    //secret
+//    query.addQueryItem("client_secret", SC_CLIENT_SECRET);
+//    //uname
+
+//    //pword
+
+//    //redirect_uri
+
+//    url.setQuery(query.query());
+
+//    QNetworkAccessManager mgr;
+//    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+//    QNetworkRequest req(url);
+//    QNetworkReply *reply = mgr.get(req);
+//    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+//    if (reply->error() == QNetworkReply::NoError) {
+//        qDebug() << "Success: " << reply->readAll();
+//        delete reply;
+
+//    }
+//    else {
+//        //failure
+//        qDebug() << "Failure" <<reply->errorString();
+//        delete reply;
+//    }
+
+//}
+
+int SCHandler::request_song(QString download_url, QString target="./"){
+
+    // create custom temporary event loop on stack
+    QNetworkRequest request;
+    request.setRawHeader("User-Agent", USER_AGENT);
+    QString local_path;
+    QEventLoop eventLoop;
+    QString req_url = download_url;
+    QUrl url(req_url);
+    QUrlQuery query;
+
+    query.addQueryItem("client_id", SC_CLIENT_ID);
+
+    url.setQuery(query.query());
+
+    // "quit()" the event-loop, when the network request "finished()"
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+    // the HTTP request
+    QNetworkRequest req(url);
+
+    //req.setRawHeader();
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+
+
+    if (reply->error() == QNetworkReply::NoError) {
+        //get temp download url as json
+
+        QJsonDocument jsondoc = QJsonDocument(QJsonDocument::fromJson(QString(reply->readAll()).toUtf8()));
+        QJsonObject jobj = jsondoc.object();
+        QString download_url = jobj["location"].toString();
+
+        //download
+        if (reply->error() == QNetworkReply::NoError) {
+            req_url = download_url;
+            QUrl url(req_url);
+            QNetworkRequest req(url);
+            req.setRawHeader("User-Agent",USER_AGENT);
+            QNetworkReply *reply = mgr.get(req);
+            eventLoop.exec();
+
+            QUrl aUrl(url);
+            QFileInfo fileInfo=aUrl.path();
+            qDebug() << fileInfo.fileName();
+            QFile file(target);
+            file.open(QIODevice::WriteOnly);
+            file.write(reply->readAll());
+            delete reply;
+            return file.size();
+
+        else{
+            qDebug() << "Failure on download request" <<reply->errorString();
+            delete reply;
+            return -1;
+        }
+
+    }
+    else {
+        //failure
+        qDebug() << "Failure on initial request" <<reply->errorString();
+        delete reply;
+        return -1;
+    }
+}
+
+
---
+++ schandler.h
@@ -0,0 +1,65 @@
+#ifndef SCHANDLER_H
+#define SCHANDLER_H
+
+#include <QString>
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+#define SC_CLIENT_ID "ae7e59c9fcb720a8c86d0d4e00d0b48b"
+#define SC_CLIENT_SECRET "e49e945b8da6f59e5602801f7f4187cb"
+#define USER_AGENT "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+#define SC_TRACKS_URL "https://api.soundcloud.com/tracks/"
+
+/*!
+ * \brief The SCHandler class is used to access the Soundcloud APIs. Allows searching and
+ *        requests for downloadable songs from Soundcloud.
+ */
+
+class SCHandler
+{
+
+private:
+    QJsonDocument last_search;
+public:
+    QJsonArray results;
+
+    SCHandler();
+    ~SCHandler();
+
+    /*!
+     \brief Queries the Soundcloud database using a key and associated
+            value. This pair corresponds to song metadata that is used
+            to request song results from Soundcloud. The search results
+            are returned are all downloadable (as set by Soundcloud) and
+            returned as an array of JSON documents (QJsonArray), populating
+            results.
+
+     \param key: A QString corresponding to the key used to define the query
+     \param value: A QString corresponding to the value used in the query
+     \return an integer value denoting the number of search results found
+             or -1 in the case of a failed query
+    */
+    int query(QString request);
+
+    //void connect(QString uname, QString pword);
+
+    /*!
+     \brief Requests a song from Soundcloud using the download_url
+            supplied by a Soundcloud query(). The value associated
+            with the download_url key provides access to an interstitial
+            url used to access the downloadable file url. This file is
+            downloaded to the specified directory or to the current directory
+            if not specified.
+
+     \param download_url: The QString value associated with the download_url found
+                          in a Soundcloud query JSON object
+     \param target: QString associated absolute or relative path (starting at the project directory)
+                    to which to save the file
+     \return an integer value denoting the size of the file or -1 for an unsuccessful request
+    */
+    int request_song(QString song_id);
+
+};
+#endif // SCHANDLER_H
